e8da77f5a9d6d3110348ca1a5eef369155a6bdb2
Sharmarke Aden <sharmarke.aden@gmail.com>
2017-03-13 11:04:29 -0700

    - added releasing and contributing documentation files - updated build script and added update secrets script - changed artifact id and package from org.testify to org.testifyproject

---
+++ storm/src/main/java/org/testifyproject/resource/storm/StormResource.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016-2017 Testify Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.testifyproject.resource.storm;
+
+import org.apache.storm.ILocalCluster;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.utils.Utils;
+import org.testifyproject.ResourceInstance;
+import org.testifyproject.ResourceProvider;
+import org.testifyproject.TestContext;
+import org.testifyproject.core.ResourceInstanceBuilder;
+import org.testifyproject.core.util.FileSystemUtil;
+import static org.testifyproject.guava.common.base.StandardSystemProperty.USER_DIR;
+
+/**
+ * An implementation of ResourceProvider that provides a local storm cluster.
+ *
+ * @author saden
+ */
+public class StormResource implements ResourceProvider<Void, ILocalCluster, Void> {
+
+    private final FileSystemUtil fileSystemUtil = FileSystemUtil.INSTANCE;
+
+    private LocalCluster localCluster;
+
+    @Override
+    public Void configure(TestContext testContext) {
+        return null;
+    }
+
+    @Override
+    public ResourceInstance<ILocalCluster, Void> start(TestContext testContext, Void config) {
+        String testName = testContext.getName();
+        String localDirectory = fileSystemUtil.createPath("target", "storm", testName);
+        fileSystemUtil.recreateDirectory(localDirectory);
+        //XXX: we have to set the user dir property to insure local cluster
+        //creates the log directories in target test folder.
+        System.setProperty(USER_DIR.key(), localDirectory);
+
+        localCluster = new LocalCluster();
+
+        return new ResourceInstanceBuilder<ILocalCluster, Void>()
+                .server(localCluster, ILocalCluster.class)
+                .build();
+    }
+
+    @Override
+    public void stop() {
+        Utils.sleep(2000);
+        localCluster.shutdown();
+    }
+
+}
